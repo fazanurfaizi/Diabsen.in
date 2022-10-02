4b23a1f2cf47110c8e6f6aebb8e0b3115ae91d74
Yayang Septian <yayang.nugraha@ovo.id>
2022-10-02 21:11:52 +0700

    Production 02102022 2111

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -5,20 +5,7 @@
 </template>
 
 <script>
-import BreadCrumb from "./BreadCrumb.vue";
-import Sidebar from "./TheSidebar.vue";
-import Header from "./TheHeader.vue";
-import SidebarMobile from "./SidebarMobile.vue";
-import Footer from "./TheFooter.vue";
-
 export default {
   name: "BaseLayout",
-  components: {
-    BreadCrumb,
-    Sidebar,
-    Header,
-    SidebarMobile,
-    Footer,
-  },
 };
 </script>
